f201468f15c5dd281a92a46faa6e5b798ce5d3b3
Emmi Kerminen <emmikerminen@Emmi-MacBook-Pro.local>
2021-01-27 19:23:17 +0200

    ulkoasun muokkaus ja js local storage

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const input = document.getElementById("textarea")
 const button = document.getElementById("submit") 
 const list = document.getElementById("lista")
 const tasks = []
+const varasto = window.localStorage 
 
 
 button.addEventListener('click', ()=> {
@@ -13,8 +14,12 @@ button.addEventListener('click', ()=> {
 })
 
 function addTask(text){
-    
     const li = document.createElement("li")
+    varasto.setItem('task', text)
     li.innerHTML = text
     list.appendChild(li) 
+}
+
+function handleLocalStorage(text){
+    const tasks = []
 }
